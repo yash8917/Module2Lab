aadd82a6a8397a42c8129b736b7cca57a41a9c5f
Yash Gupta <yash8917gupta@gmail.com>
2021-11-11 15:21:14 +0530

    initial commit

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class ExceptionHandling {
     public static void main(String[] args) {
-        int a=90;
+        Scanner s=new Scanner(System.in);
+
+        int a= s.nextInt();
+        int b=s.nextInt();
+        int arr[]={12,21,34,54,45};
+        int index =s.nextInt();
 //         arthematic exception
-        System.out.println(a/0);
+        try{
+            System.out.println(a/b);
+            System.out.println(arr[index]);
+        }
+        catch (ArrayIndexOutOfBoundsException obj){
+            System.out.println(obj);
+        }
+        catch (ArithmeticException obj){
+            System.out.println(obj);// or we can write it as obj only it will givre the discription and definition
+        }
+        catch (Exception obj){
+            System.out.println(obj);
+        }
         System.out.println("end of the main.");
     }
 }
