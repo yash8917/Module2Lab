604afb31ecba8c2f655be46847f3fca948ea67d6
Yash Gupta <yash8917gupta@gmail.com>
2021-11-11 14:29:25 +0530

    initial commit

--- src/WrapperClasses.java
+++ src/WrapperClasses.java
@@ -1,9 +1,12 @@
 public class WrapperClasses {
     public static void main(String[] args) {
-        Integer i1=23;//this is boxing
-        int res=i1.intValue();//this is a processess of unboxing
-        System.out.println(res +3);
-        String str =i1.toString();
-        System.out.println(str +7);
+        Integer i1=34;//this is boxing
+//        int res=i1.intValue();//this is a processess of unboxing
+//        System.out.println(res +3);
+//        String str =i1.toString();//it will concatinate of the string
+//        System.out.println(str +7);
+        //-----------------------------------
+        Integer i2=90;
+        System.out.println(i1 + i2);
     }
 }
