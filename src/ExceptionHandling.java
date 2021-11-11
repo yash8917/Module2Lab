6165a4e4f4bdfbb82f3b1afccc48652aa2d958eb
Yash Gupta <yash8917gupta@gmail.com>
2021-11-11 14:49:57 +0530

    initial commit

---
+++ src/ExceptionHandling.java
@@ -0,0 +1,8 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int a=90;
+//         arthematic exception
+        System.out.println(a/0);
+        System.out.println("end of the main.");
+    }
+}
