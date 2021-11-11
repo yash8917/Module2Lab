4a8dd0d79f17f856ac10e43f31db6d9b0b5bad88
Yash Gupta <yash8917gupta@gmail.com>
2021-11-11 14:25:27 +0530

    Initial commit

---
+++ src/WrapperClasses.java
@@ -0,0 +1,9 @@
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Integer i1=23;//this is boxing
+        int res=i1.intValue();//this is a processess of unboxing
+        System.out.println(res +3);
+        String str =i1.toString();
+        System.out.println(str +7);
+    }
+}
